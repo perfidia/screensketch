5388e5bcd6f3638418636075b0cf2e3b78f2e4ca
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2013-04-12 20:20:40 +0200

    add tests for XMLWriter

--- tests/screenspec/reader/xml.py
+++ tests/screenspec/reader/xml.py
@@ -9,6 +9,8 @@ import unittest
 from StringIO import StringIO
 from screensketch.screenspec.reader import TextReader
 from screensketch.screenspec.writer import TextWriter
+from screensketch.screenspec.reader import XMLReader
+from screensketch.screenspec.writer import XMLWriter
 
 class Test(unittest.TestCase):
 	@classmethod
@@ -32,18 +34,22 @@ class Test(unittest.TestCase):
 	def wrapper(self, filename):
 		tmp_fd = StringIO()
 
-		input = open(filename).read()
-		output = TextReader(input).execute()
+		input_text = open(filename).read()
+		input_model = TextReader(input_text).execute()
 
-		tmp0 = input.split('\n')
+		XMLWriter(input_model).execute(tmp_fd)
+		output_model = XMLReader(tmp_fd.getvalue()).execute()
 
-		TextWriter(output).execute(tmp_fd)
+		tmp_fd = StringIO()
+		TextWriter(output_model).execute(tmp_fd)
+
+		tmp0 = input_text.split('\n')
 		tmp1 = tmp_fd.getvalue().split('\n')
 
 		self.assertEqual(len(tmp0), len(tmp1))
 
-		for idx, val in enumerate(tmp0):
-			self.assertEqual(val, tmp1[idx])
+		for i, j in zip(tmp0, tmp1):
+			self.assertEqual(i, j)
 
 	def testExample01(self):
 		self.wrapper(self.path + "/example01.txt")
